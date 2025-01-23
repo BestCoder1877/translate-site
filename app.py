600a1275f59213466bb5ddfe8a0ac744c5e53937
BestCoder1877 <153776092+BestCoder1877@users.noreply.github.com>
2025-01-23 14:05:22 +0000

    Added loading bar

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
 from batchTranslate import translate
-from flask import Flask,request,session,render_template,redirect
+from flask import Flask,request,session,render_template
 from os import getenv
+
+def formatStr(text):
+        return text.replace('\n','<br>')
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'hello'
 
-@app.route("/debug")
-def debug():
-    return "Suscsessfull"
+
+
+@app.route("/theportaltotheotherworld")
+def theportaltotheotherworld():
+    return "In the portal to the other world, you can get rick rolled when you find the seacret"
 
 @app.route("/", methods=["GET", "POST"])
 def select():
     if request.method == 'POST':
-        session["text"] = request.form['text'].upper()
-        session["times"] = request.form["times"].upper()
-        return redirect("/get")
+        session["text"] = request.form['text']
+        session["times"] = request.form["times"]
+        try:
+            session["times"]=int(session["times"])
+        except:
+            return "Please enter a number"
+        if session["times"]<=0:
+            return "Please enter a number greater than 0"
+        session["resualt"]=translate(session["times"],session["text"])
+        return render_template("done.html", resault=formatStr(session["resualt"]))
     return render_template("index.html")
 
-@app.route("/get",methods=["GET"])
-def get():
-    resualt=translate(int(session["times"]),session["text"])
-    return resualt
+                    
+@app.route("/done",methods=["GET"])
+def done():
+    return render_template("done.html",done=session["resualt"])
 
 if __name__ == "__main__":
     app.run(debug=False,port=int(getenv('PORT', 8080)),host="0.0.0.0")
